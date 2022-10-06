4c3b09b389fcd19dcba1c89631bc5829d6a35337
LAladder <42671607+LAladder@users.noreply.github.com>
2022-10-06 14:10:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'ccc9f2a538df62dde5b76363703bf2a4',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山西',
+  CITY: '太原',
 
   USERS: [
     {
